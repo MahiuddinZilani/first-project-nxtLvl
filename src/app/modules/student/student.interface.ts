dd46367c61198246bf1c9a6a1c0cfd5c44adfaf9
MahiuddinZilani <zilani.cse.cou@gmail.com>
2024-06-29 02:43:57 +0600

    student features added

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,40 @@
+// import { Schema, model, connect } from 'mongoose';
+
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherPhone: string;
+  motherName: string;
+  motherOccupation: string;
+  motherPhone: string;
+};
+
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  phone: string;
+  address: string;
+};
+
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  dob?: Date;
+  email: string;
+  phone: string;
+  emergencyPhone: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImage?: string;
+  isActive: 'active' | 'inActive';
+};
